c0f01b9330f0b04f847655aa50504555c106267f
silvodesigns <kevsilvo@gmail.com>
2019-10-14 19:44:16 -0400

    Quick unit testing fix

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import App from './App';
+import  useFetch from './useFetch.js'
 
+test('make sure it returns a defined value',()=>{
+  expect(useFetch).toBeDefined();
+})
 
-it('the app element has been rendered', () => {
-  const wrapper = render(
-    <App/>
-  )
-  console.log(wrapper.debug())
-});
+test('make sure the length of the array if greater than 0',()=>{
+  expect(useFetch).not.toHaveLength(0);
+})
+
